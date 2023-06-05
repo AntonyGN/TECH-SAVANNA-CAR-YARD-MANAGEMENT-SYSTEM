a37e712a24fc10991e53f7855918afa21f1cab36
Antony Githendu <66016848+AntonyGN@users.noreply.github.com>
2023-06-05 11:56:15 +0300

    Update index.blade.php

--- resources/views/cardetails/index.blade.php
+++ resources/views/cardetails/index.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
 
 @section('content')
@@ -7,13 +6,14 @@
         <div class="row">
             <div class="col-md-11">
                 <div class="card">
-                    <div class="card-header">Car-details</div>
-                    <div class="card-body">
-                        <a href="{{ url('/cardetails/create') }}" class="btn btn-success btn-sm" title="Add New detail">
+                    <div class="card-header">
+                        <h1 class="float-left">Car details</h1>
+                        <a href="{{ url('/cardetails/create') }}" class="btn btn-success btn-sm float-right" title="Add New detail">
                             <i class="fa fa-plus" aria-hidden="true"></i> Add New
                         </a>
-                        <br/>
-                        <br/>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="card-body">
                         <div class="table-responsive">
                             <table class="table">
                                 <thead>
@@ -25,30 +25,40 @@
                                         <th>YearofManufacture</th>
                                         <th>Transmission</th>
                                         <th>ChassisNo</th>
+                                        <th>Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                @foreach($cardetails as $item)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $item->make }}</td>
-                                        <td>{{ $item->model }}</td>
-                                        <td>{{ $item->fueltype  }}</td>
-                                        <td>{{ $item->yearofmanufacture}}</td>
-                                        <td>{{ $item->transmission }}</td>
-                                        <td>{{ $item->chassisno }}</td>
-
-                                        <td>
-                                            <a href="{{ url('/cardetails/' . $item->id) }}" title="View Car"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
-                                            <a href="{{ url('/cardetails/' . $item->id . '/edit') }}" title="Edit Car"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
-                                            <form method="POST" action="{{ url('/cardetails' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
-                                                {{ method_field('DELETE') }}
-                                                {{ csrf_field() }}
-                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Car" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                    @foreach($cardetails as $item)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $item->make }}</td>
+                                            <td>{{ $item->model }}</td>
+                                            <td>{{ $item->fueltype  }}</td>
+                                            <td>{{ $item->yearofmanufacture}}</td>
+                                            <td>{{ $item->transmission }}</td>
+                                            <td>{{ $item->chassisno }}</td>
+                                            <td>
+                                                <a href="{{ url('/cardetails/' . $item->id) }}" title="View Car">
+                                                    <button class="btn btn-info btn-sm">
+                                                        <i class="fa fa-eye" aria-hidden="true"></i> View
+                                                    </button>
+                                                </a>
+                                                <a href="{{ url('/cardetails/' . $item->id . '/edit') }}" title="Edit Car">
+                                                    <button class="btn btn-primary btn-sm">
+                                                        <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
+                                                    </button>
+                                                </a>
+                                                <form method="POST" action="{{ url('/cardetails' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                                    {{ method_field('DELETE') }}
+                                                    {{ csrf_field() }}
+                                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete Car" onclick="return confirm(&quot;Confirm delete?&quot;)">
+                                                        <i class="fa fa-trash-o" aria-hidden="true"></i> Delete
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
