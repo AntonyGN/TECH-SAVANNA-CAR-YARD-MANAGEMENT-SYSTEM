a1d374e66883a23feff9293c417153ac7a4a2d4c
Antony Githendu <66016848+AntonyGN@users.noreply.github.com>
2023-06-12 16:46:14 +0300

    Update show.blade.php

--- resources/views/Inventory/show.blade.php
+++ resources/views/Inventory/show.blade.php
@@ -1,25 +1,25 @@
-@extends('inventory.layout')
-@section('content')
- 
- 
-<div class="card">
-  <div class="card-header">Inventory</div>
-  <div class="card-body">
-   
- 
-        <div class="card-body">
-        <h5 class="card-title">Cartype : {{ $inventory->cartype }}</h5>
-        <p class="card-text">Carmodel : {{ $inventory->carmodel }}</p>
-        <p class="card-text">Availablecars : {{ $inventory->availablecars }}</p>
-        <p class="card-text">Soldcars : {{ $inventory->soldcars }}</p>
-       
-        
-                                        
 
-       
-  </div>
-       
-    </hr>
-  
-  </div>
-</div>
+@extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
+
+@section('content')
+    @include('layouts.navbars.auth.topnav', ['title' => 'Your Profile'])
+    <div class="container">
+        <div class="row">
+            <div class="col-md-11">
+                <div class="card">
+                    <div class="card-header"><h1>Inventory</h1></div>
+                    <div class="card-body">
+                        <div class="card-body">
+                            <h5 class="card-title">Cartype: {{ $inventory->cartype }}</h5>
+                            <p class="card-text">Carmodel: {{ $inventory->carmodel }}</p>
+                            <p class="card-text">Availablecars: {{ $inventory->availablecars }}</p>
+                            <p class="card-text">Soldcars: {{ $inventory->soldcars }}</p>
+                        </div>
+                    </div>
+                </div>
+                
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
