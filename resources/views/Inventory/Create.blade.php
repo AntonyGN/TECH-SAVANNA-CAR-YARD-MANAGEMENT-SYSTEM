835926d47373217c7c1b5a74f783cca643b58811
Antony Githendu <66016848+AntonyGN@users.noreply.github.com>
2023-06-12 16:41:40 +0300

    Update Create.blade.php

--- resources/views/Inventory/Create.blade.php
+++ resources/views/Inventory/Create.blade.php
@@ -1,23 +1,32 @@
-@extends('inventory.layout')
+
+@extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
+
 @section('content')
-<div class="card">
-  <div class="card-header">Car Inventory Page</div>
-  <div class="card-body">
-      
-      <form action="/cars/store" method="post">
-        {!! csrf_field() !!}
-        <label>Car type</label></br>
-        <input type="text" name="cartype" id="cartype" class="form-control"></br>
-        <label>Car model</label></br>
-        <input type="text" name="carmodel" id="carmodel" class="form-control"></br>
-        <label>Available cars</label></br>
-        <input type="text" name="availablecars" id="availablecars" class="form-control"></br>
-        <label>Sold cars</label></br>
-        <input type="text" name="soldcars" id="soldcars" class="form-control"></br>
-        
-        <input type="submit" value="Save" class="btn btn-success"></br>
-    </form>
-  
-  </div>
-</div>
-@stop
+    @include('layouts.navbars.auth.topnav', ['title' => 'Your Profile'])
+    <div class="container">
+        <div class="row">
+            <div class="col-md-11">
+              <div class="card">
+                <div class="card-header"><h1>Car Inventory Page</h1></div>
+                <div class="card-body">
+                  <form action="/inventory/store" method="post">
+                    {!! csrf_field() !!}
+                      <label>Car type</label><br>
+                      <input type="text" name="cartype" id="cartype" class="form-control"><br>
+                      <label>Car model</label><br>
+                      <input type="text" name="carmodel" id="carmodel" class="form-control"><br>
+                      <label>Available cars</label><br>
+                      <input type="text" name="availablecars" id="availablecars" class="form-control"><br>
+                      <label>Sold cars</label><br>
+                      <input type="text" name="soldcars" id="soldcars" class="form-control"><br>
+              
+                      <input type="submit" value="Save" class="btn btn-success"><br>
+                  </form>
+                
+                </div>
+              </div>
+              
+            </div>
+        </div>
+    </div>
+@endsection
