8fd8b2ac7bf32247826cc96f4cf4eb972602d183
Antony Githendu <66016848+AntonyGN@users.noreply.github.com>
2023-06-12 16:38:06 +0300

    Update InventoryController.php

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -7,54 +7,63 @@ use Illuminate\Http\Request;
 
 class InventoryController extends Controller
 {
-
     public function index()
     {
         $inventory = Inventory::all();
         return view('inventory.index')->with('inventory', $inventory);
     }
-    
+
     public function create()
     {
-        return view('inventory.create');
+        return view('inventory.Create');
     }
- 
-  
+
     public function store(Request $request)
     {
+        $request->validate([
+            'cartype' => 'required',
+            'carmodel' => 'required',
+            'availablecars' => 'required',
+            'soldcars' => 'required',
+        ]);
+
         $input = $request->all();
-        Contact::create($input);
-        return redirect('inventory')->with('flash_message', 'Inventory Addedd!');  
+        Inventory::create($input);
+        return redirect('inventory')->with('flash_message', 'Inventory Added!');
     }
- 
-    
+
     public function show($id)
     {
         $inventory = Inventory::find($id);
-        return view('inventory.show')->with('inventory', $contact);
+        return view('inventory.show')->with('inventory', $inventory);
     }
- 
-    
+
     public function edit($id)
     {
         $inventory = Inventory::find($id);
-        return view('inventory.edit')->with('inventory', $contact);
+        return view('inventory.edit')->with('inventory', $inventory);
     }
- 
-  
+
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'cartype' => 'required',
+            'carmodel' => 'required',
+            'availablecars' => 'required',
+            'soldcars' => 'required',
+        ]);
+
         $inventory = Inventory::find($id);
         $input = $request->all();
-        $contact->update($input);
-        return redirect('inventory')->with('flash_message', 'Inventory Updated!');  
+        $inventory->update($input);
+        return redirect('inventory')->with('flash_message', 'Inventory Updated!');
     }
- 
-  
+
     public function destroy($id)
     {
-        inventory::destroy($id);
-        return redirect('inventory')->with('flash_message', 'Inventory deleted!');  
+        Inventory::destroy($id);
+        return redirect('inventory')->with('flash_message', 'Inventory deleted!');
     }
 }
+
 
